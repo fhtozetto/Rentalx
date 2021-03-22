59c86efa3c020fdf9fcc2b90d7cd1fe7dacbc514
Fernando Hnerique Tozetto <fhtozetto@gmail.com>
2021-03-22 00:15:46 -0300

    Reading the upload data in the repository

--- src/modules/cars/useCases/importCategory/index.ts
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -1,8 +1,9 @@
+import { CategoriesRepository } from '../../repositories/implementations/CategoriesRepository';
 import { ImportCategoryController } from './ImportCategoryController';
 import { ImportCategoryUseCAse } from './ImportCategoryUseCase';
 
-const importCategoryUseCAse = new ImportCategoryUseCAse();
-
+const categoriesRepository = CategoriesRepository.getInstance();
+const importCategoryUseCAse = new ImportCategoryUseCAse(categoriesRepository);
 const importCategoryController = new ImportCategoryController(
   importCategoryUseCAse,
 );
