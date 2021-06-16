b2cc3294fb4ca304fd1b308cf84d81583c87bb9f
Fernando Hnerique Tozetto <fhtozetto@gmail.com>
2021-06-16 00:49:07 -0300

    Refactoring user authentication

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: '94d36da456061600c0f27ca5d24a8c0e',
+  secret_refresh_token: 'ee178ffec03d2f434e157d4cfd18d2a0',
+  expires_in_token: '15m',
+  expires_in_refresh_token: '30d',
+  expires_refresh_token_days: 30,
+};
