e8b169c95b1535a00740d7db96d024b618382a4a
Fernando Hnerique Tozetto <fhtozetto@gmail.com>
2021-06-17 23:25:28 -0300

    Creating refresh token use case

---
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenController.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from 'express';
+
+class RefreshTokenController {
+  async handler(request: Request, response: Response): Promise<Response> {}
+}
+
+export { RefreshTokenController };
